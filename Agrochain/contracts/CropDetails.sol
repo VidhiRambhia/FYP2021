3a53b9bac3e627f51d039fd20729b1ebfab51e19
Vruddhi Mehta <vgmehta_b17@ce.vjti.ac.in>
2020-12-23 20:21:44 +0530

    Crop Details SC

---
+++ Agrochain/contracts/CropDetails.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.7.0;
+
+struct Crop{
+    uint cropID;
+    string cropType;
+    string cropName;
+    string cropVariety;
+    string fertiliser;
+    string season;
+    string sourceTagNo;
+    uint quantity;
+    uint256 DateSowed;
+    uint256 DateHarvested;
+    string cropRegistrationCode;
+}
+
+contract CropDetails{
+    
+    //event when farmer is added
+    event CropAdded(address indexed account);
+    event CropUpdated(address indexed account);
+    
+    mapping(address=>Crop[]) crops;
+    
+    function addCrop1(string calldata cropType, string calldata cropName, string calldata cropVariety, string calldata sourceTagNo,
+        string calldata cropRegistrationCode, address farmerAddr) public returns (uint cropID){
+            Crop memory crop;
+            crop.cropID = crops[farmerAddr].length;
+            crop.cropType = cropType;
+            crop.cropName = cropName;
+            crop.cropVariety = cropVariety;
+            crop.sourceTagNo = sourceTagNo;
+            crop.cropRegistrationCode = cropRegistrationCode;
+            crops[farmerAddr].push(crop);
+            return crop.cropID;
+        }
+        
+    function addCrop2(uint cropID, string memory fertiliser, string memory season, uint quantity, uint256 DateSowed,
+    uint256 DateHarvested, address farmerAddr) public{
+            Crop storage crop = crops[farmerAddr][cropID];
+            crop.fertiliser = fertiliser;
+            crop.season = season;
+            crop.quantity = quantity;
+            crop.DateSowed = DateSowed;
+            crop.DateHarvested = DateHarvested;
+            emit CropAdded(farmerAddr);
+        }
+        
+    function updateHarvestDate(uint cropID, address farmerAddr, uint256 DateHarvested) public{
+        Crop storage crop = crops[farmerAddr][cropID];
+        crop.DateHarvested = DateHarvested;
+        emit CropUpdated(farmerAddr);
+    }
+    
+    function getCrop1(address farmerAddr, uint cropID) public returns (string memory cropType, string memory cropName,
+    string memory cropVariety, string memory sourceTagNo, string memory cropRegistrationCode){
+        return (crops[farmerAddr][cropID].cropType, crops[farmerAddr][cropID].cropName,
+            crops[farmerAddr][cropID].cropVariety, crops[farmerAddr][cropID].sourceTagNo,
+            crops[farmerAddr][cropID].cropRegistrationCode);
+        }
+        
+    function getCrop2(address farmerAddr, uint cropID) public returns (string memory fertiliser, string memory season,
+    uint quantity, uint256 DateSowed, uint256 DateHarvested){
+        return (crops[farmerAddr][cropID].fertiliser, crops[farmerAddr][cropID].season, crops[farmerAddr][cropID].quantity,
+            crops[farmerAddr][cropID].DateSowed, crops[farmerAddr][cropID].DateHarvested);
+    }
+}
