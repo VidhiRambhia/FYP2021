daf962809e5e87095b3f784aaf08d3bcaba24a04
riya-shah06 <rnshah_b17@ce.vjti.ac.in>
2020-12-23 19:29:20 +0530

    Added basic routes

--- Agrochain/main.py
+++ Agrochain/main.py
@@ -10,7 +10,7 @@ print(w3.isConnected())
 
 # Initialize a local account object from the private key of a valid Ethereum node address
 # Add your own private key here
-local_acct = w3.eth.account.from_key("ddff7ae4990cca4baf964997c1092bdecb505556e5b5a2fbf020f6b3102b61bd")
+local_acct = w3.eth.account.from_key("3b4a60ab47652548c19663d1ae02703da2eb2f92434f304d87f12abc5fad567b")
 
 # compile your smart contract with truffle first
 truffleFile = json.load(open('./build/contracts/Migrations.json'))
@@ -22,7 +22,7 @@ contract = w3.eth.contract(bytecode=bytecode, abi=abi)
 
 # build a transaction by invoking the buildTransaction() method from the smart contract constructor function
 # Add your own account address here
-construct_txn = contract.constructor(60, '0x23881459cF4C4B8bE41f45980452c803e9A18445').buildTransaction({
+construct_txn = contract.constructor(60, '0x8E73855be1b32A8a3693eB2b15503EDd51939a3E').buildTransaction({
     'from': local_acct.address,
     'nonce': w3.eth.getTransactionCount(local_acct.address),
     'gas': 1728712,
@@ -54,6 +54,22 @@ def index():
     print(contract_address)
     print(w3.isConnected())
     return render_template('index.html')
+
+@app.route("chooseRole", methods=["GET"])
+def chooseRole():
+    return render_template('ChooseRole.html')
+
+@app.route("cropDetails")
+def cropDetails():
+    return render_template('CropDetails.html')
+
+@app.route("farmerPage")
+def farmerPage():
+    return render_template('FarmerFunctions.html')
+
+@app.route("login")
+def login():
+    return render_template("Login.html")
 
 
 @app.route("/error")
