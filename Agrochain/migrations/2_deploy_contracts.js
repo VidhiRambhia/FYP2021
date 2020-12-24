3169685de7132eeeedfb5181961337866bc1ca0e
Vruddhi Mehta <vgmehta_b17@ce.vjti.ac.in>
2020-12-24 12:29:13 +0530

    Compiled Contracts

--- Agrochain/migrations/2_deploy_contracts.js
+++ Agrochain/migrations/2_deploy_contracts.js
@@ -1,8 +1,8 @@
-const SimpleAuction = artifacts.require("SimpleAuction");
+const Migrations = artifacts.require("Migrations");
 
 module.exports = function (deployer) {
   biddingTime = 3000;
   beneficiary = '0x6Ef4724927cca14ac42Cb775fB55a6ab65451A8E';
-  deployer.deploy(SimpleAuction,biddingTime,beneficiary);
+  deployer.deploy(Migrations,biddingTime,beneficiary);
 };
 
