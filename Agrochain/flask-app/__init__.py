5540c5c44429e03ac1464218dd08b6c93ce92f36
vidhi2601 <vsrambhia@gmail.com>
2020-12-28 01:54:14 +0530

    Deleting duplicate  files

--- Agrochain/flask-app/__init__.py
+++
@@ -1,6 +0,0 @@
-# Statement for enabling the development environment
-DEBUG = True
-
-# Define the application directory
-import os
-BASE_DIR = os.path.abspath(os.path.dirname(__file__)) 
--- Agrochain/flask-app/dapp/configurations/config.py
+++
@@ -1,4 +0,0 @@
-local_acct_key = '3b4a60ab47652548c19663d1ae02703da2eb2f92434f304d87f12abc5fad567b'
-cropDetails_contract_address = '0xeA6711362F52a3949c888D10474Df86B1e1dE3Dc'
-farmerDetails_contract_address = '0x2475965f81ae0f32bC6FEDC1d08C785Daf7cD3b8'
-login_contract_address = '0x88e5Ffc6c7e9DFe4df0F98EC5724478C019B007E'
