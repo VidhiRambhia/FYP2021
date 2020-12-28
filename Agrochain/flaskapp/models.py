87d4f09bd56c3b5e9398544148eafab2426b92dd
vidhi2601 <vsrambhia@gmail.com>
2020-12-28 13:20:06 +0530

    Displaying buttons on home page selectively after login

--- Agrochain/flaskapp/models.py
+++ Agrochain/flaskapp/models.py
@@ -6,6 +6,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     address = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    #role = 
 
     def __repr__(self):
         return f"{self.email}"
