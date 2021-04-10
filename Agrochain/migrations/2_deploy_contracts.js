903a88b78773c809deca45f0d4a19cdc65571dc1
VidhiRambhia <vsrambhia@gmail.com>
2021-04-10 13:46:08 +0530

    Adding deployment script for contracts

--- Agrochain/migrations/2_deploy_contracts.js
+++ Agrochain/migrations/2_deploy_contracts.js
@@ -1,10 +1,16 @@
 const CropDetails = artifacts.require("CropDetails");
 const FarmerDetails = artifacts.require("FarmerDetails");
-const User = artifacts.require("Login");
+const FpcDetails = artifacts.require("FpcDetails");
+const RetailerDetails = artifacts.require("RetailerDetails");
+const LogisticsDetails = artifacts.require("LogisticsDetails");
+const TransactionDetails = artifacts.require("TransactionDetails");
 
 module.exports = function (deployer) {
   deployer.deploy(CropDetails);
-  deployer.deploy(FarmerDetails);
-  deployer.deploy(User);  
+  deployer.deploy(FarmerDetails); 
+  deployer.deploy(FpcDetails);
+  deployer.deploy(RetailerDetails);
+  deployer.deploy(LogisticsDetails);
+  deployer.deploy(TransactionDetails);
 };
 
