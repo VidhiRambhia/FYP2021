a19550128aa11ef810897be74f2e89a10e534e3e
Vruddhi Mehta <vgmehta_b17@ce.vjti.ac.in>
2021-04-10 16:10:19 +0530

    Bug fixing and linking routes

--- Agrochain/flaskapp/commonRoutes.py
+++ Agrochain/flaskapp/commonRoutes.py
@@ -3,15 +3,16 @@ import sys
 import datetime
 import hashlib
 import os
-from web3 import Web3, HTTPProvider,IPCProvider
+from web3 import Web3, HTTPProvider, IPCProvider
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import Flask,Blueprint, render_template, request, redirect, url_for,flash
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import UserMixin, LoginManager, login_user, login_required, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from flaskapp.config import config
 from flaskapp.models import User
-from flaskapp import db,w3,eth,app
+from flaskapp import db, w3, eth, app
 import random
+import string
 
 mod_common = Blueprint('common', __name__, url_prefix='')
 
@@ -25,17 +26,16 @@ def load_user(user_id):
 
 
 @mod_common.route("/home")
-@mod_common.route("/", methods=["GET","POST"])
+@mod_common.route("/", methods=["GET", "POST"])
 def index():
     if current_user:
-        return render_template('home.html',current_user=current_user)
+        return render_template('home.html', current_user=current_user)
     return render_template('home.html')
 
 
-
-@mod_common.route("/chooseRole", methods=["GET","POST"])
+@mod_common.route("/chooseRole", methods=["GET", "POST"])
 def chooseRole():
-    if(request.method=="POST"):
+    if(request.method == "POST"):
         selectedRole = request.form.get('role')
         print('selected role: ', selectedRole)
         if (selectedRole == 'farmer'):
@@ -61,13 +61,14 @@ def login():
             return redirect(url_for('common.login'))
 
         login_user(user, remember=False)
-        
+
         # Conditions for different entities
         if(current_user.role == 'FARMER'):
             return redirect(url_for('farmer.farmerPage'))
         elif(current_user.role == 'FPC'):
             return redirect(url_for('fpc.fpcPage'))
     return render_template("Login.html")
+
 
 @mod_common.route("/logout")
 @login_required
@@ -80,34 +81,40 @@ def logout():
 def error():
     return render_template('error.html')
 
-@mod_common.route("/addTransactionDetails",methods=["GET","POST"])
+
+@mod_common.route("/addTransactionDetails", methods=["GET", "POST"])
 @login_required
 def addTransactionDetails():
-    buyer_name = request.form.get('buyer_name')
-    seller_name = request.form.get('seller_name')
-    crop_name = request.form.get('crop_name')
-    product_grade = request.form.get('product_grade')
-    cost = float(request.form.get('cost'))
-    quantity = request.form.get('quantity')
-    package_id = buyer_name[:2] + seller_name[:2] + str(randint())
-    #logistic id
-    #prev_hash
+    if request.method == 'POST':
+        buyer_name = request.form.get('buyer_name')
+        seller_name = request.form.get('seller_name')
+        crop_name = request.form.get('crop_name')
+        product_grade = request.form.get('product_grade')
+        cost = request.form.get('cost')
+        quantity = request.form.get('quantity')
+        package_id = buyer_name[:2].upper(
+        ) + seller_name[:2].upper() + str(random.randint(1, 100000))
+        print(package_id)
+    if 'next' in request.form:
+        return redirect(url_for('common.logistics', package_id=package_id))
+    # connect with SC
+    return render_template('addTransactionDetails.html', current_user=current_user)
 
-    #connect with SC
-    return render_template('addTransactionDetails.html',current_user=current_user,transaction=transaction)
 
-@mod_common.route("/logistics", methods=["GET","POST"])
+@mod_common.route("/logistics", methods=["GET", "POST"])
 @login_required
 def logistics():
-    if request.method == 'POST': 
+    package_id = request.args.get('package_id')
+    print(package_id)
+    if request.method == 'POST':
         vehicle_type = request.form.get('vehicle_type')
         l_id = request.form.get('l_id')
         vehicle_number = request.form.get('vehicle_number')
         driver_name = request.form.get('driver_name')
         driver_contact = request.form.get('driver_contact')
         dispatch_date = request.form.get('dispatch_date')
-        dispatch_date = datetime.datetime(*[int(item) for item in dispatch_date.split('-')])
+        dispatch_date = datetime.datetime(
+            *[int(item) for item in dispatch_date.split('-')])
         dispatch_date_int = int(dispatch_date.strftime('%Y%m%d'))
-        print(dispatch_date)
         # Connect with SC
-    return render_template('logistics.html')
+    return render_template('logistics.html', package_id=package_id)
